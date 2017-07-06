7bffc64d466bd12f5d233febd5da803b9932dbee
Jesse Stockall <jstockall@embotics.com>
2017-07-05 20:09:51 -0400

    add 404 test

--- users/users_test.go
+++ users/users_test.go
@@ -53,7 +53,7 @@ func TestGetUsers(t *testing.T) {
     }
 }
 
-func TestGetUserById(t *testing.T) {
+func TestGetUserById_Exists(t *testing.T) {
     // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
     // pass 'nil' as the third parameter.
     req, err := http.NewRequest("GET", "/users/58b1ec0ccd02960006e0fcde", nil)
@@ -80,5 +80,32 @@ func TestGetUserById(t *testing.T) {
 
     if actual != expected {
       t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+    }
+}
+
+func TestGetUserById_404(t *testing.T) {
+    // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+    // pass 'nil' as the third parameter.
+    req, err := http.NewRequest("GET", "/users/11b1ec0ccd02960006e0fcde", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+    rr := httptest.NewRecorder()
+
+    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+    // directly and pass in our Request and ResponseRecorder.
+    router.ServeHTTP(rr, req)
+
+    // Check the status code is what we expect.
+    if status := rr.Code; status != http.StatusNotFound {
+        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+    }
+
+    // Check the response body is empty
+    actual := rr.Body.String()
+    if actual != "" {
+      t.Errorf("handler returned unexpected body: got [%v] want no body", actual)
     }
 }
